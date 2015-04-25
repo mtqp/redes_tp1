f672a0d2bbfea93919a5fa7dc1866f843f6525a5
mtqp <marian_sabianaa@hotmail.com>
2015-04-25 14:40:35 -0300

    add entropy
    procesamiento de probabilidad y entropia para cada fuente

--- source.py
+++ source.py
@@ -1,22 +1,39 @@
+import math
 
 class source:
     def __init__(self):
         self.count = 0
-        self.packets = dict()
+        self.messages = dict()
         
-    def add_packet(self, key):
+    def add(self, message):
         self.count += 1
-        if self.packets.has_key(key):
-            self.packets[key] += 1
+        if self.messages.has_key(message):
+            self.messages[message] += 1
         else:
-            self.packets[key] = 1
+            self.messages[message] = 1
     
-    def save(self, statistics_file, header, key_parser):
-        all_keys = self.packets.keys()
-        all_keys.sort()
+    def save(self, statistics_file, header, parse):
+        all_messages = self.messages.keys()
+        all_messages.sort()
 
+        entropy = 0
+        base_2 = 2
+        header += ",Probability\n"
+        
         csv = open(statistics_file,"w")
-        csv.write(header + "\n")
-        for key in all_keys:
-            csv.write(key_parser(key) + "," + str(self.packets[key]) + "\n")
+        csv.write(header)
+        for message in all_messages:
+            message_count = self.messages[message]
+            message_probability = float(message_count)/self.count
+            
+            message_entropy = message_probability * math.log(message_probability, base_2)
+            entropy -= message_entropy
+            
+            #print "Entropy of: " + parse(message) + " is: " + str(message_entropy)
+            #print "Entropy so far is: " + str(entropy)
+            
+            csv.write(parse(message) + "," + str(self.messages[message]) + "," + str(message_probability) + "\n")
+            
+        csv.write("Entropy\n")
+        csv.write(str(entropy))
         csv.close()
--- source_analizer.py
+++ source_analizer.py
@@ -1,17 +1,28 @@
 import sys
 from scapy.all import *
+from datetime import datetime, timedelta
 from source import * 
 
 type_source = source()
 arp_source = source()
 
-def analize_arp_packet(packet):
+def print_source_update(processing_source):
+    if (processing_source.count % 10) == 0:
+        print "Reached " + str(processing_source.count) + " packets processed at: " + str(datetime.now().time())
+
+def extract_only_arp_packet(packet):
     if packet.type == int(0x806):
         op_code = packet[ARP].op
         source = packet[ARP].psrc
         destination = packet[ARP].pdst
-        arp_source.add_packet((op_code, source, destination))
 
+        arp_source.add((op_code, source, destination))
+        print_source_update(arp_source)
+        
+def extract_type_from_packet(packet):
+    type_source.add(packet.type)
+    print_source_update(type_source)
+    
 def type_key_parser(type):
     return hex(type)
     
@@ -21,13 +32,13 @@ def arp_key_parser(arp_key):
 def sniff_types(source_file, statistics_file):
     header = "Type,Count"
     
-    sniff(prn = lambda pkt : type_source.add_packet(pkt.type), offline = source_file)
+    sniff(prn = extract_type_from_packet, offline = source_file)
     type_source.save(statistics_file, header, type_key_parser)
         
 def sniff_arp(source_file, statistics_file):
     header = "OpCode,Source,Destination,Count"
     
-    sniff(prn = analize_arp_packet, offline = source_file, filter="arp")
+    sniff(prn = extract_only_arp_packet, offline = source_file, filter="arp")
     arp_source.save(statistics_file, header, arp_key_parser)
 
         
